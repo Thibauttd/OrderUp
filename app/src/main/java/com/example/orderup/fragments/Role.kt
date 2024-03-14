7faacb0d57dee68dd511ecf817fd9ea1c4adbc22
Votre Nom <Vous@exemple.com>
2024-03-14 21:35:09 +0100

    fixer taille ecran

--- app/src/main/java/com/example/orderup/fragments/Role.kt
+++ app/src/main/java/com/example/orderup/fragments/Role.kt
@@ -1,4 +1,4 @@
-package com.example.orderup.fragments
+package com.example.orderup.activity
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,11 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.orderup.R
+import com.example.orderup.databinding.FormulesBinding // Import correct binding class
 import com.example.orderup.databinding.RoleBinding
 
 /**
- * A simple [Fragment] subclass serving as the default destination in the navigation.
- * This fragment allows users to select their role in the application.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class Role : Fragment() {
 
@@ -24,7 +24,7 @@ class Role : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
 
         _binding = RoleBinding.inflate(inflater, container, false) // Correct binding class name
         return binding.root
@@ -34,22 +34,18 @@ class Role : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Navigate to the previous screen
         binding.btnPrecedent.setOnClickListener {
-            findNavController().navigate(R.id.action_Role_to_Carte)
+            findNavController().navigate(R.id.action_Role_to_Formules)
         }
 
-        // Navigate to the manager role screen
         binding.iconeGerant.setOnClickListener {
             findNavController().navigate(R.id.action_Role_to_Gerant)
         }
 
-        // Navigate to the cook role screen
         binding.iconeCuisinier.setOnClickListener {
             findNavController().navigate(R.id.action_Role_to_Cook)
         }
 
-        // Navigate to the waiter role screen
         binding.iconeServeur.setOnClickListener {
             findNavController().navigate(R.id.action_Role_to_Serveur)
         }
