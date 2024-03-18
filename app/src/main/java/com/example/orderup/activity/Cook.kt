6a1207e29ed030a10629a0f7a8688c286a3d200c
yoann <yoannchiron@outlook.fr>
2024-03-18 16:16:20 +0100

    affichage nom + quantité pour cuisinier

--- app/src/main/java/com/example/orderup/activity/Cook.kt
+++ app/src/main/java/com/example/orderup/activity/Cook.kt
@@ -1,12 +1,12 @@
 package com.example.orderup.activity
 
+import CookAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.orderup.adaptater.CookAdapter
 import com.example.orderup.databinding.CookBinding
 import com.example.orderup.model.OrderModel
 import com.example.orderup.repository.OrderRepository
